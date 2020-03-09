668b9c7f8423faca70cd11ba0dfe81e5c7ea44c3
Anna Nikiforovskaya <nikiannanik@gmail.com>
2020-03-10 00:26:12 +0300

    Add hw03

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -10,6 +10,7 @@ import numpy as np
 import sortednp as snp
 import cv2
 import click
+import pims
 
 from corners import CornerStorage
 from _corners import filter_frame_corners
@@ -25,13 +26,51 @@ from _camtrack import (
     triangulate_correspondences,
     TriangulationParameters,
     rodrigues_and_translation_to_view_mat3x4,
-    pose_to_view_mat3x4
+    pose_to_view_mat3x4,
+    eye3x4
 )
 
 
-MAX_PROJ_ERROR = 1.0
-MIN_TRIANG_ANG = 1.0
-MIN_DEPTH = 0.1
+MAX_PROJ_ERROR = 3.0
+MIN_TRIANG_ANG = 5
+MIN_DEPTH = 0.00001
+
+
+def init_tracking(intrinsic_mat: np.ndarray,
+                  corner_storage: CornerStorage,
+                  rgb_sequence: pims.FramesSequence):
+    triang_params = TriangulationParameters(max_reprojection_error=MAX_PROJ_ERROR, 
+                                            min_triangulation_angle_deg=5, 
+                                            min_depth=MIN_DEPTH)
+    #print(np.cos(np.deg2rad(5)))
+    frame_1 = 0
+    view_mat_1 = eye3x4()
+    min_cos = 1
+    max_len_ps = 0
+    for i in range(1, len(rgb_sequence)): # Ok, I would like to choose the best pair
+        # First, find the similar points in the two pictures
+        correspondences = build_correspondences(corner_storage[frame_1], corner_storage[i])
+        if len(correspondences.ids) < 10:
+            continue
+        # Next, compute fundamental matrix
+        E, mask = cv2.findEssentialMat(correspondences.points_1,
+                                       correspondences.points_2,
+                                       intrinsic_mat,
+                                       threshold=MAX_PROJ_ERROR) # RANSAC inside by default
+        retval, R, t, mask = cv2.recoverPose(E, correspondences.points_1, correspondences.points_2)
+        view_mat_2 = np.hstack((R, -t))
+        points, ids, median_cos = triangulate_correspondences(correspondences, 
+                                                              view_mat_1, view_mat_2,
+                                                              intrinsic_mat, 
+                                                              triang_params)
+        min_cos = min(min_cos, median_cos)
+        max_len_ps = max(max_len_ps, len(points))
+        print(f"\r{i} {retval} {len(points)} {median_cos}", end="")
+        if len(points) > 10:
+            print()
+            return (frame_1, view_mat3x4_to_pose(view_mat_1)), (i, view_mat3x4_to_pose(view_mat_2)) 
+    print("\n", min_cos, max_len_ps)
+    return None, None
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
@@ -40,14 +79,21 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None) \
         -> Tuple[List[Pose], PointCloud]:
-    if known_view_1 is None or known_view_2 is None:
-        raise NotImplementedError()
 
     rgb_sequence = frameseq.read_rgb_f32(frame_sequence_path)
     intrinsic_mat = to_opencv_camera_mat3x3(
         camera_parameters,
         rgb_sequence[0].shape[0]
     )
+
+    if known_view_1 is None or known_view_2 is None:
+        known_view_1, known_view_2 = init_tracking(intrinsic_mat, corner_storage, rgb_sequence)
+        if known_view_1 is None:
+            print("Something went wrong.")
+            exit(0)
+
+    print(f"Got start frames: {known_view_1[0]} {known_view_2[0]}")
+    
 
     triang_params = TriangulationParameters(max_reprojection_error=MAX_PROJ_ERROR, 
                                             min_triangulation_angle_deg=MIN_TRIANG_ANG, 
@@ -82,7 +128,8 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                 retval, rvec, tvec, inliers = cv2.solvePnPRansac(point_cloud_builder.points[idx_1],
                                                                  corners.points[idx_2],
                                                                  intrinsic_mat,
-                                                                 distCoeffs=None)
+                                                                 distCoeffs=None,
+                                                                 reprojectionError=MAX_PROJ_ERROR)
                 inliers = np.array(inliers, dtype=int)
                 if len(inliers) > 0:
                     view_mats[i] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
@@ -92,12 +139,12 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                 print(f"\rFrame {i} out of {len(rgb_sequence)}, inliners: {0}", end="")
             if view_mats[i] is None:
                 continue
-            cur_corner = filter_frame_corners(corner_storage[i], inliers.flatten())
+            #cur_corner = filter_frame_corners(corner_storage[i], inliers.flatten())
             
             for j in range(len(rgb_sequence)):
                 if view_mats[j] is None:
                     continue
-                correspondences = build_correspondences(corner_storage[j], cur_corner)
+                correspondences = build_correspondences(corner_storage[j], corner_storage[i])
                 if len(correspondences.ids) == 0:
                     continue
                 points, ids, median_cos = triangulate_correspondences(correspondences, 
